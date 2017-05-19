24fae332d74703b096a1a38370b24f462883a987
Alfredo-AMA <alfredo.h.delos_santos@ama-inc.com>
2017-05-19 11:50:24 -0400

    Used nan_to_num rather than explicit casting to zero, removed 'optional' clear mask.

--- dc_tsm.py
+++ dc_tsm.py
@@ -7,19 +7,12 @@ from . import dc_utilities as utilities
 from datetime import datetime
 
 
-####################################################
-# ｜ ＴＳＭ ｜
-####################################################
-# 0.0001 for the scale of ls7 data.
 def _tsmi(dataset):
     return (dataset.red.astype('float64') + dataset.green.astype('float64')) * 0.0001 / 2
 
 
 def tsm(dataset_in, clean_mask=None, no_data=0):
-    # Create a clean mask from cfmask if the user does not provide one
-    if clean_mask is None:
-        cfmask = dataset_in.cf_mask
-        clean_mask = utilities.create_cfmask_clean_mask(cfmask)
+    assert clean_mask is not None, "Please supply a boolean clean mask with the same dimensions as dataset_in"
 
     tsm = 3983 * _tsmi(dataset_in)**1.6246
     tsm.values[np.invert(clean_mask)] = no_data  # Contains data for clear pixels
@@ -33,7 +26,7 @@ def tsm(dataset_in, clean_mask=None, no_data=0):
 
 
 def mask_tsm(dataset_in, wofs):
-    wofs_criteria = wofs.copy(deep=True).normalized_data.where(wofs.normalized_data > 0.8)
+    wofs_criteria = wofs.where(wofs > 0.8)
     wofs_criteria.values[wofs_criteria.values > 0] = 0
     kernel = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]])
 
@@ -43,7 +36,6 @@ def mask_tsm(dataset_in, wofs):
     dataset_out = dataset_in.copy(deep=True)
     dataset_out.normalized_data.values += mask
     dataset_out.total_clean.values += mask
-    dataset_out.normalized_data.values[np.isnan(dataset_out.normalized_data.values)] = 0
-    dataset_out.total_clean.values[np.isnan(dataset_out.total_clean.values)] = 0
+    utilities.nan_to_num(dataset_out, 0)
 
     return dataset_out
