7a2df51e708653e49b7e54f2aaedf7d413c24abe
Alfredo-AMA <alfredo.h.delos_santos@ama-inc.com>
2017-08-03 16:37:52 -0400

    Updated tsm to be a more general water quality module.

---
+++ data_cube_utilities/dc_water_quality.py
@@ -0,0 +1,82 @@
+import gc
+import numpy as np
+import xarray as xr
+import scipy.ndimage.filters as conv
+
+from . import dc_utilities as utilities
+from datetime import datetime
+
+
+def _tsmi(dataset):
+    return (dataset.red.astype('float64') + dataset.green.astype('float64')) * 0.0001 / 2
+
+
+def tsm(dataset_in, clean_mask=None, no_data=0):
+    assert clean_mask is not None, "Please supply a boolean clean mask with the same dimensions as dataset_in"
+    assert 'red' in dataset_in and 'green' in dataset_in, "Red and Green bands are required for the TSM analysis."
+
+    tsm = 3983 * _tsmi(dataset_in)**1.6246
+    tsm.values[np.invert(clean_mask)] = no_data  # Contains data for clear pixels
+
+    # Create xarray of data
+    time = dataset_in.time
+    latitude = dataset_in.latitude
+    longitude = dataset_in.longitude
+    dataset_out = xr.Dataset({'tsm': tsm}, coords={'time': time, 'latitude': latitude, 'longitude': longitude})
+    return dataset_out
+
+
+def mask_water_quality(dataset_in, wofs):
+    wofs_criteria = wofs.where(wofs > 0.8)
+    wofs_criteria.values[wofs_criteria.values > 0] = 0
+    kernel = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]])
+
+    mask = conv.convolve(wofs_criteria.values, kernel, mode='constant')
+    mask = mask.astype(np.float32)
+
+    dataset_out = dataset_in.copy(deep=True)
+    dataset_out.normalized_data.values += mask
+    dataset_out.total_clean.values += mask
+    utilities.nan_to_num(dataset_out, 0)
+
+    return dataset_out
+
+
+def watanabe_chlorophyll(dataset_in, clean_mask=None, no_data=0):
+    assert clean_mask is not None, "Please supply a boolean clean mask with the same dimensions as dataset_in"
+    assert 'red' in dataset_in and 'nir' in dataset_in, "Red and NIR bands are required for the Watanabe Chlorophyll analysis."
+
+    chl_a = 925.001 * (dataset_in.nir.astype('float64') / dataset_in.red.astype('float64')) - 77.16
+    chl_a.values[np.invert(clean_mask)] = no_data  # Contains data for clear pixels
+
+    # Create xarray of data
+    time = dataset_in.time
+    latitude = dataset_in.latitude
+    longitude = dataset_in.longitude
+    dataset_out = xr.Dataset(
+        {
+            'watanabe_chlorophyll': chl_a
+        }, coords={'time': time,
+                   'latitude': latitude,
+                   'longitude': longitude})
+    return dataset_out
+
+
+def nazeer_chlorophyll(dataset_in, clean_mask=None, no_data=0):
+    assert clean_mask is not None, "Please supply a boolean clean mask with the same dimensions as dataset_in"
+
+    chl_a = (0.57 * (dataset_in.red.astype('float64') * 0.0001) /
+             (dataset_in.blue.astype('float64') * 0.0001)**2) - 2.61
+    chl_a.values[np.invert(clean_mask)] = no_data  # Contains data for clear pixels
+
+    # Create xarray of data
+    time = dataset_in.time
+    latitude = dataset_in.latitude
+    longitude = dataset_in.longitude
+    dataset_out = xr.Dataset(
+        {
+            'nazeer_chlorophyll': chl_a
+        }, coords={'time': time,
+                   'latitude': latitude,
+                   'longitude': longitude})
+    return dataset_out
