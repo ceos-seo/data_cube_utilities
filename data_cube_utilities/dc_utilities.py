89d635dc4204e723db7c89a880f1d84787c4dfb9
John C. Rattz <jcrattz@bigdata-node3.internal.ama-inc.com>
2018-04-26 10:10:16 -0400

    Added method that creates default clean mask - masking nothing

--- data_cube_utilities/dc_utilities.py
+++ data_cube_utilities/dc_utilities.py
@@ -56,6 +56,23 @@ def create_cfmask_clean_mask(cfmask, no_data=-9999):
     clean_mask = (cfmask == 0) | (cfmask == 1)
     return clean_mask.values
 
+def create_default_clean_mask(dataset_in):
+    """
+    Description:
+        Creates a data mask that masks nothing.
+    -----
+    Inputs:
+        dataset_in (xarray.Dataset) - dataset retrieved from the Data Cube.
+    Throws:
+        ValueError - if dataset_in is an empty xarray.Dataset.
+    """
+    data_vars = dataset_in.data_vars
+    if len(data_vars) != 0:
+        first_data_var = next(iter(data_vars))
+        clean_mask = np.ones(dataset_in[first_data_var].shape).astype(np.bool)
+        return clean_mask
+    else:
+        raise ValueError('`dataset_in` has no data!')
 
 def get_spatial_ref(crs):
     """
