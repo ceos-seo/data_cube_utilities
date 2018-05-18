5ebb02018ac6d608ff0030c2823c96a3b1ef1e29
John C. Rattz <jcrattz@bigdata-node3.internal.ama-inc.com>
2018-05-18 16:55:16 -0400

    Added create_2D_mosaic_clean_mask() to clean_mask.py

--- data_cube_utilities/clean_mask.py
+++ data_cube_utilities/clean_mask.py
@@ -2,6 +2,28 @@
 from utils.data_cube_utilities.dc_mosaic import (ls7_unpack_qa, ls8_unpack_qa, ls5_unpack_qa)
 import numpy as np
 
+def create_2D_mosaic_clean_mask(clean_mask):
+    """
+    The clean mask of a mosaic should be determined by the compositing function (e.g. mean 
+    mosaic, median mosaic, etc.). This is simply supposed to be a decent approximation of a 
+    clean mask for a mosaic that has no time dimension.
+    
+    Parameters
+    ----------
+    clean_mask: np.ndarray
+        The 3D clean mask used to construct the mosaic.
+    
+    Returns
+    -------
+    mosaic_clean_mask: np.ndarray
+        A 2D clean mask for a mosaic.
+    """
+    mosaic_clean_mask = clean_mask[0]
+    # Take the logical OR of clean masks through time.
+    for i in range(1, clean_mask.shape[0]):
+        mosaic_clean_mask = np.logical_or(mosaic_clean_mask, clean_mask[i])    
+    return mosaic_clean_mask
+
 
 def landsat_qa_clean_mask(dataset, platform):
     """
@@ -10,9 +32,17 @@ def landsat_qa_clean_mask(dataset, platform):
     Parameters
     ----------
     dataset: xarray.Dataset
-        An xarray (usually produced by `datacube.load()`) that contains a `pixel_qa` data variable.
+        An xarray (usually produced by `datacube.load()`) that contains a `pixel_qa` data 
+        variable.
     platform: str
-        A string denoting the platform to be used. Can be "LANDSAT_5", "LANDSAT_7", or "LANDSAT_8".
+        A string denoting the platform to be used. Can be "LANDSAT_5", "LANDSAT_7", or 
+        "LANDSAT_8".
+        
+    Returns
+    -------
+    clean_mask: numpy.ndarray
+        A numpy array with the same number and order of dimensions as the coordinates of 
+        `dataset`.
     """
     processing_options = {
         "LANDSAT_5": ls5_unpack_qa,
