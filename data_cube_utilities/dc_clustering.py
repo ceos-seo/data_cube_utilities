1b69e5851ef2cb9c7b8d7c99463bc8f3007969ec
John C. Rattz <jcrattz@bigdata-node3.internal.ama-inc.com>
2018-05-24 11:58:04 -0400

    Refactored some clustering functions to reduce code duplication.

--- data_cube_utilities/dc_clustering.py
+++ data_cube_utilities/dc_clustering.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 from sklearn.manifold import TSNE
-from sklearn.cluster import KMeans, AgglomerativeClustering, DBSCAN
+from sklearn.cluster import KMeans, AgglomerativeClustering, DBSCAN, Birch
 import numpy as np
 from collections import OrderedDict
 import xarray as xr
 import matplotlib.pyplot as plt
 
-def kmeans_cluster_dataset(dataset_in, bands=['red', 'green', 'blue', 'swir1', 'swir2', 'nir'], n_clusters=4):
+def clustering_pre_processing(dataset_in, bands):
     array_from = []
     for band in bands:
         array_from.append(dataset_in[band].values.flatten())
@@ -15,12 +15,9 @@ def kmeans_cluster_dataset(dataset_in, bands=['red', 'green', 'blue', 'swir1', '
     np_array = np.swapaxes(np_array, 0, 1)
 
     np.set_printoptions(suppress=True)
-    """
-    classified = AgglomerativeClustering(n_clusters=16).fit(np_array)
-    classified = DBSCAN(eps=0.005, min_samples=5, n_jobs=-1).fit(np_array)
-    """
-    classified = KMeans(n_clusters=n_clusters, n_jobs=-1).fit(np_array)
+    return array_from, np_array
 
+def clustering_post_processing(classified, dataset_in, bands):
     classified_data = OrderedDict()
 
     classification = classified.labels_.reshape((dataset_in[bands[0]].shape[0], dataset_in[bands[0]].shape[1]))
@@ -29,8 +26,27 @@ def kmeans_cluster_dataset(dataset_in, bands=['red', 'green', 'blue', 'swir1', '
     dataset_out = xr.Dataset(
         classified_data, coords={'latitude': dataset_in.latitude,
                                  'longitude': dataset_in.longitude})
-
     return dataset_out
+
+def kmeans_cluster_dataset(dataset_in, bands=['red', 'green', 'blue', 'swir1', 'swir2', 'nir'], n_clusters=4):
+    array_from, np_array = clustering_pre_processing(dataset_in, bands)
+    """
+    classified = AgglomerativeClustering(n_clusters=n_clusters).fit(np_array)
+    classified = Birch(n_clusters=n_clusters).fit(np_array)
+    classified = DBSCAN(eps=0.005, min_samples=5, n_jobs=-1).fit(np_array)
+    """    
+    classified = KMeans(n_clusters=n_clusters, n_jobs=-1).fit(np_array)
+    return clustering_post_processing(classified, dataset_in, bands)
+
+def birch_cluster_dataset(dataset_in, bands=['red', 'green', 'blue', 'swir1', 'swir2', 'nir'], n_clusters=4):
+    array_from, np_array = clustering_pre_processing(dataset_in, bands)
+    """
+    classified = AgglomerativeClustering(n_clusters=n_clusters, n_jobs=-1).fit(np_array)
+    classified = DBSCAN(eps=0.005, min_samples=5, n_jobs=-1).fit(np_array)
+    classified = KMeans(n_clusters=n_clusters, n_jobs=-1).fit(np_array)
+    """  
+    classified = Birch(n_clusters=n_clusters, threshold=0.00001).fit(np_array)
+    return clustering_post_processing(classified, dataset_in, bands)
 
 def plot_kmeans_next_to_mosaic(da_a, da_b):  
     def mod_rgb(dataset,
