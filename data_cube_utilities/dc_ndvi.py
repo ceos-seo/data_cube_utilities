eaf024b7f5b1d17414181d0737ba1744accfe7f5
JohnRattz <john.c.rattz@ama-inc.com>
2018-06-28 13:45:08 -0400

    Changed nodata to no_data in data_cube_utilities.dc_ndvi.compute_ndvi_anomaly()

--- data_cube_utilities/dc_ndvi.py
+++ data_cube_utilities/dc_ndvi.py
@@ -30,7 +30,7 @@ def compute_ndvi_anomaly(baseline_data,
                          scene_data,
                          baseline_clear_mask=None,
                          selected_scene_clear_mask=None,
-                         nodata=-9999):
+                         no_data=-9999):
     """Compute the scene+baseline median ndvi values and the difference
 
     Args:
@@ -38,7 +38,7 @@ def compute_ndvi_anomaly(baseline_data,
         scene_data: xarray dataset with dims lat, lon - should be mosaicked already.
         baseline_clear_mask: boolean mask signifying clear pixels for the baseline data
         selected_scene_clear_mask: boolean mask signifying lcear pixels for the baseline data
-        nodata: nodata value for the datasets
+        no_data: nodata value for the datasets
 
     Returns:
         xarray dataset with scene_ndvi, baseline_ndvi(median), ndvi_difference, and ndvi_percentage_change.
@@ -47,23 +47,23 @@ def compute_ndvi_anomaly(baseline_data,
     assert selected_scene_clear_mask is not None and baseline_clear_mask is not None, "Both the selected scene and baseline data must have associated clear mask data."
 
     #cloud filter + nan out all nodata.
-    baseline_data = baseline_data.where((baseline_data != nodata) & baseline_clear_mask)
+    baseline_data = baseline_data.where((baseline_data != no_data) & baseline_clear_mask)
 
     baseline_ndvi = (baseline_data.nir - baseline_data.red) / (baseline_data.nir + baseline_data.red)
     median_ndvi = baseline_ndvi.median('time')
 
     #scene should already be mosaicked.
     water_class = wofs_classify(scene_data, clean_mask=selected_scene_clear_mask, mosaic=True).wofs
-    scene_cleaned = scene_data.copy(deep=True).where((scene_data != nodata) & (water_class == 0))
+    scene_cleaned = scene_data.copy(deep=True).where((scene_data != no_data) & (water_class == 0))
     scene_ndvi = (scene_cleaned.nir - scene_cleaned.red) / (scene_cleaned.nir + scene_cleaned.red)
 
     ndvi_difference = scene_ndvi - median_ndvi
     ndvi_percentage_change = (scene_ndvi - median_ndvi) / median_ndvi
 
     #convert to conventional nodata vals.
-    scene_ndvi.values[~np.isfinite(scene_ndvi.values)] = nodata
-    ndvi_difference.values[~np.isfinite(ndvi_difference.values)] = nodata
-    ndvi_percentage_change.values[~np.isfinite(ndvi_percentage_change.values)] = nodata
+    scene_ndvi.values[~np.isfinite(scene_ndvi.values)] = no_data
+    ndvi_difference.values[~np.isfinite(ndvi_difference.values)] = no_data
+    ndvi_percentage_change.values[~np.isfinite(ndvi_percentage_change.values)] = no_data
 
     scene_ndvi_dataset = xr.Dataset(
         {
