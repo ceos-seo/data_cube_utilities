4f810fbaac42b2eb7d9035da7629c670e13f1a2e
John Rattz <john.c.rattz@ama-inc.com>
2018-09-21 15:37:45 -0400

    dc_rgb.py: Added ability to supply figure and axes arguments to rgb(), which now returns the figure and axes used to plot and does not call plt.show().

--- data_cube_utilities/dc_rgb.py
+++ data_cube_utilities/dc_rgb.py
@@ -2,11 +2,11 @@ import matplotlib.pyplot as plt
 from time import time
 import numpy as np
 
-from .plotter_utils import figure_ratio, xarray_set_axes_labels
+from .plotter_utils import figure_ratio, xarray_set_axes_labels, retrieve_or_create_fig_ax
 
 # Change the bands (RGB) here if you want other false color combinations
 def rgb(dataset, at_index = 0, bands = ['red', 'green', 'blue'], paint_on_mask = [],
-        min_possible = 0, max_possible = 10000, width = 10):
+        min_possible = 0, max_possible = 10000, width = 10, fig=None, ax=None):
     """
     Creates a figure showing an area, using three specified bands as the rgb componenets.
     
@@ -27,16 +27,23 @@ def rgb(dataset, at_index = 0, bands = ['red', 'green', 'blue'], paint_on_mask =
         For example, for Landsat this is generally 10000.
     width: int
         The width of the figure in inches.
+    fig: matplotlib.figure.Figure
+        The figure to use for the plot.
+        If only `fig` is supplied, the Axes object used will be the first.
+    ax: matplotlib.axes.Axes
+        The axes to use for the plot.
+        
+    Returns
+    -------
+    fig, ax: matplotlib.figure.Figure, matplotlib.axes.Axes
+        The figure and axes used for the plot.
     """
-    min_brightness = 0.2
-    
     ### < Dataset to RGB Format, needs float values between 0-1 
     rgb = np.stack([dataset[bands[0]],
                     dataset[bands[1]],
                     dataset[bands[2]]], axis = -1)
     # Interpolate values to be in the range [0,1] for creating the image.
     rgb = np.interp(rgb, (np.nanmin(rgb), np.nanmax(rgb)), [0,1])
-    # TODO: Mask out shadows.
     ### > 
     
     ### < takes a T/F mask, apply a color to T areas  
@@ -44,13 +51,13 @@ def rgb(dataset, at_index = 0, bands = ['red', 'green', 'blue'], paint_on_mask =
         rgb[mask] = np.array(color)/ 255.0
     ### > 
     
-    fig, ax = plt.subplots(figsize = figure_ratio(rgb.shape[:2], fixed_width = width))
+    fig, ax = retrieve_or_create_fig_ax(fig, ax, figsize=figure_ratio(rgb.shape[:2], fixed_width = width))
 
     xarray_set_axes_labels(dataset, ax)
    
     if 'time' in dataset.dims:
-        plt.imshow(rgb[at_index])
+        ax.imshow(rgb[at_index])
     else:
-        plt.imshow(rgb)
+        ax.imshow(rgb)
     
-    plt.show()
+    return fig, ax
