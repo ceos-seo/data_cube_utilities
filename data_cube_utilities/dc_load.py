561037fcd990d9bb6282ea4dceae916f1de09498
jcrattz <john.c.rattz@ama-inc.com>
2018-10-01 12:14:43 -0400

    dc_load.py: Updated merge_datasets(), load_simple(), and load_multiplatform() to return masks of single cover types, like water and cloud, if specified.

--- data_cube_utilities/dc_load.py
+++ data_cube_utilities/dc_load.py
@@ -25,32 +25,40 @@ def xarray_concat_and_merge(*args, concat_dim='time', sort_dim='time'):
     """
     merged = []
     for i, arg in enumerate(args):
-        current_concat_dim = concat_dim[i] if isinstance (concat_dim, list) else concat_dim
-        current_sort_dim = sort_dim[i] if isinstance (sort_dim, list) else sort_dim
+        current_concat_dim = concat_dim[i] if isinstance(concat_dim, list) else concat_dim
+        current_sort_dim = sort_dim[i] if isinstance(sort_dim, list) else sort_dim
         dataset_temp = xr.concat(arg, dim=concat_dim)
         merged.append(xarray_sortby_coord(dataset_temp, coord=sort_dim))
     return merged
 
-def merge_datasets(datasets_temp, clean_masks_temp):
+def merge_datasets(datasets_temp, clean_masks_temp, masks_per_platform=None):
     """
-    Merges dictionaries of platforms mapping to datasets and 
-    dataset masks into one dataset and dataset mask.
+    Merges dictionaries of platforms mapping to datasets, dataset clean masks,
+    and lists of other masks into one dataset, one dataset clean mask, and one
+    of each type of other mask, ordering all by time.
 
     Parameters
     ----------
-    datasets_temp, clean_masks_temp: dict
-        Maps platforms to datasets to merge and dataset masks to merge.
+    datasets_temp, clean_masks_temp, masks_per_platform: dict
+        Maps platforms to datasets to merge, dataset masks to merge,
+        and lists of masks to merge separately, respectively.
+        All entries must have a 'time' dimension.
 
     Returns
     -------
     dataset: xarray.Dataset
-        The data requested.
+        The raw data requested. Can be cleaned with `dataset.where(clean_mask)`.
     clean_mask: xarray.DataArray
-        The clean mask, formed as a logical AND of all masks used.
+        The clean mask.
+    masks: list of xarray.DataArray
+        A list of individual masks.
 
     Raises
     ------
     AssertionError: If no data was retrieved for any query (i.e. `len(datasets_temp) == 0`).
+    
+    :Authors:
+        John Rattz (john.c.rattz@ama-inc.com)
     """
     assert len(datasets_temp) > 0, "No data was retrieved." # No data for any query.
     # If multiple non-empty datasets were retrieved, merge them and sort by time.
@@ -63,12 +71,22 @@ def merge_datasets(datasets_temp, clean_masks_temp):
         clean_masks_temp_list = list(clean_masks_temp.values())
         clean_mask = xr.concat(clean_masks_temp_list, dim='time')
         clean_mask = xarray_sortby_coord(clean_mask, 'time')
+        # Merge masks.
+        num_platforms = len(masks_per_platform.keys())
+        num_masks = len(list(masks_per_platform.values())[0])
+        np_platform_masks = np.empty((num_platforms, num_masks), dtype=object)
+        for i, mask_list in enumerate(masks_per_platform.values()):
+            np_platform_masks[i] = mask_list
+        masks = []
+        for j in range(num_masks):
+            masks.append(xr.concat(list(np_platform_masks[:,j]), dim='time'))
     else: # Select the only dataset.
         dataset = datasets_temp[list(datasets_temp.keys())[0]]
         clean_mask = clean_masks_temp[list(clean_masks_temp.keys())[0]]
-    return dataset, clean_mask
+        masks = masks_per_platform[list(masks_per_platform.keys())[0]]
+    return dataset, clean_mask, masks
 
-def load_simple(dc, platform, product, load_params={}, masking_params={}):
+def load_simple(dc, platform, product, load_params={}, masking_params={}, indiv_masks=None):
     """
     Simplifies loading from the Data Cube by retrieving a dataset along with its mask.
 
@@ -89,33 +107,48 @@ def load_simple(dc, platform, product, load_params={}, masking_params={}):
           'LANDSAT_8': dict(common_load_params, **dict(time=ls8_date_range))}`, where `common_load_params` is 
         a dictionary of load parameters common to both - most notably 'lat', 'lon', and 'measurements'.
     masking_params: dict, optional
-        A dictionary mapping platform names to a dictionary of keyword arguments for corresponding masking functions.
-        For example: {'LANDSAT_7': {'cover_types':['cloud']}, 'LANDSAT_8': {'cover_types': ['cloud']}} would retain
-        only clouds, because `landsat_qa_clean_mask()` is used for the Landsat family of platforms.
+        A dictionary of keyword arguments for corresponding masking functions.
+        For example: {'cover_types':['cloud']} would retain only clouds for Landsat products, 
+        because `landsat_qa_clean_mask()` is used for the Landsat family of platforms.
+    indiv_masks: list
+        A list of masks to return (e.g. ['water']). 
+        These do not have to be the same used to create `clean_mask`.
 
     Returns
     -------
     dataset: xarray.Dataset
-        The data requested.
+        The raw data requested. Can be cleaned with `dataset.where(clean_mask)`.
     clean_mask: xarray.DataArray
         The clean mask, formed as a logical AND of all masks used.
+    masks: list of xarray.DataArray
+        A list of the masks requested by `indiv_masks`, 
+        or `None` if `indiv_masks` is not specified.
 
     Raises
     ------
-    AssertionError: If no data is retrieved for this query.
+    AssertionError: If no data is retrieved for any platform query.
+    
+    :Authors:
+        John Rattz (john.c.rattz@ama-inc.com)
     """
     current_load_params = dict(platform=platform, product=product)
-    current_masking_params = masking_params.get(platform, {})
     current_load_params.update(load_params)
     dataset = dc.load(**current_load_params)
-    assert len(dataset.dims) > 0, "No data was retrieved." # No data for this query.
+    assert len(dataset.dims) > 0, "No data was retrieved."
     # Get the clean mask for the appropriate LANDSAT satellite platform.
-    clean_mask = landsat_qa_clean_mask(dataset, platform, **current_masking_params)
+    clean_mask = landsat_qa_clean_mask(dataset, platform, **masking_params)
     # Get the mask for removing data ouside the accepted range of LANDSAT 7 and 8.
     clean_mask = xr_and(clean_mask, landsat_clean_mask_invalid(dataset))
-    return dataset, clean_mask
+    # Retrieve individual masks.
+    if indiv_masks is None:
+        masks = None
+    else:
+        masks = []
+        for mask in indiv_masks:
+            masks.append(landsat_qa_clean_mask(dataset, platform, cover_types=[mask]))
+    return dataset, clean_mask, masks
 
-def load_multiplatform(dc, platforms, products, load_params={}, masking_params={}):
+def load_multiplatform(dc, platforms, products, load_params={}, masking_params={}, indiv_masks=None):
     """
     Load and merge data as well as clean masks, given a list of platforms and products.
     Currently only tested on Landsat data.
@@ -141,54 +174,73 @@ def load_multiplatform(dc, platforms, products, load_params={}, masking_params={
         A dictionary mapping platform names to a dictionary of keyword arguments for corresponding masking functions.
         For example: {'LANDSAT_7': {'cover_types':['cloud']}, 'LANDSAT_8': {'cover_types': ['cloud']}} would retain
         only clouds, because `landsat_qa_clean_mask()` is used for the Landsat family of platforms.
+    indiv_masks: list
+        A list of masks to return (e.g. ['water']). 
+        These do not have to be the same used to create `clean_mask`.
     
     Returns
     -------
     dataset: xarray.Dataset
-        The data requested.
+        The raw data requested. Can be cleaned with `dataset.where(clean_mask)`.
     clean_mask: xarray.DataArray
         The clean mask, formed as a logical AND of all masks used.
+    masks: list of xarray.DataArray
+        A list of the masks requested by `indiv_masks`, 
+        or `None` if `indiv_masks` is not specified.
         
     Raises
     ------
     AssertionError: If no data is retrieved from any product.
+    
+    :Authors:
+        John Rattz (john.c.rattz@ama-inc.com)
     """
     datasets_temp = {} # Maps platforms to datasets to merge.
     clean_masks_temp = {} # Maps platforms to clean masks to merge.
+    masks_per_platform = {} if indiv_masks is not None else None # Maps platforms to lists of masks.
     for product,platform in zip(products, platforms):
         current_load_params = dict(platform=platform, product=product)
-        current_masking_params = masking_params.get(platform, {})
+        current_masking_params = masking_params.get(platform, masking_params)
         
         # Handle `load_params` as a dict of dicts of platforms mapping to load params.
         if isinstance(list(load_params.values())[0], dict): 
             current_load_params.update(load_params.get(platform, {}))
         else: # Handle `load_params` as a dict of load params.
             current_load_params.update(load_params)
-        
         # Load each time range of data.
         time = current_load_params.get('time')
-        if isinstance(time[0], list): # Handle `time` as a list of time ranges.
+        if isinstance(time[0], tuple) or \
+           isinstance(time[0], list): # Handle `time` as a list of time ranges.
             datasets_time_parts = []
             clean_masks_time_parts = []
-            for time_range in time:
+            masks_time_parts = np.empty((len(time), len(indiv_masks)), dtype=object)\
+                               if indiv_masks is not None else None
+            for i, time_range in enumerate(time):
                 time_range_load_params = current_load_params
                 time_range_load_params['time'] = time_range
                 try:
-                    dataset_time_part, clean_mask_time_part = \
-                        load_simple(dc, platform, product, time_range_load_params, masking_params)
+                    dataset_time_part, clean_mask_time_part, masks_time_part = \
+                        load_simple(dc, platform, product, time_range_load_params, 
+                                    masking_params, indiv_masks)
                     datasets_time_parts.append(dataset_time_part)
-                    clean_masks_time_parts.append(clean_mask_time_part)    
+                    clean_masks_time_parts.append(clean_mask_time_part)
+                    if indiv_masks is not None:
+                        masks_time_parts[i] = masks_time_part
                 except (AssertionError):
                     continue
             datasets_temp[platform], clean_masks_temp[platform] = \
                 xarray_concat_and_merge(datasets_time_parts, clean_masks_time_parts)
+            if indiv_masks is not None:
+                masks_per_platform[platform] = xarray_concat_and_merge(*masks_time_parts.T)
         else: # Handle `time` as a single time range.
             try:
-                datasets_temp[platform], clean_masks_temp[platform] = \
-                    load_simple(dc, platform, product, load_params, masking_params)
+                datasets_temp[platform], clean_masks_temp[platform], masks = \
+                    load_simple(dc, platform, product, current_load_params, masking_params, indiv_masks)
+                if indiv_masks is not None:
+                    masks_per_platform[platform] = masks
             except (AssertionError):
                 continue
-    return merge_datasets(datasets_temp, clean_masks_temp)
+    return merge_datasets(datasets_temp, clean_masks_temp, masks_per_platform)
 
 def get_overlapping_area(api, platforms, products):
     """
@@ -205,6 +257,8 @@ def get_overlapping_area(api, platforms, products):
     -------
     full_lat, full_lon: tuple
         Two 2-tuples of the minimum and maximum latitude and longitude, respectively.
+    min_max_dates: tuple
+        A 2-tuple of the minimum and maximum time available to all products.
     """
     min_max_dates = np.empty((len(platforms), 2), dtype=object)
     min_max_lat = np.empty((len(platforms), 2))
@@ -225,4 +279,4 @@ def get_overlapping_area(api, platforms, products):
     max_lat = np.min(min_max_lat[:,1])
     full_lon = (min_lon, max_lon)
     full_lat = (min_lat, max_lat)
-    return full_lat, full_lon
+    return full_lat, full_lon, min_max_dates
