126ed458f2d7825a139464552126482f502c320b
John Rattz <john.c.rattz@ama-inc.com>
2018-10-25 12:32:18 -0400

    dc_rgb.py: Added 4 parameters to rgb() to allow tweaking of the resulting image intensity.

--- data_cube_utilities/dc_rgb.py
+++ data_cube_utilities/dc_rgb.py
@@ -5,8 +5,11 @@ import numpy as np
 from .plotter_utils import figure_ratio, xarray_set_axes_labels, retrieve_or_create_fig_ax
 
 # Change the bands (RGB) here if you want other false color combinations
-def rgb(dataset, at_index = 0, bands = ['red', 'green', 'blue'], paint_on_mask = [],
-        min_possible = 0, max_possible = 10000, width = 10, fig=None, ax=None):
+def rgb(dataset, at_index=0, x_coord='longitude', y_coord='latitude',
+        bands=['red', 'green', 'blue'], paint_on_mask = [],
+        min_possible=0, max_possible=10000, use_data_min=False,
+        use_data_max=False, min_inten=0.15, max_inten=1.0,
+        width=10, fig=None, ax=None):
     """
     Creates a figure showing an area, using three specified bands as the rgb componenets.
     
@@ -16,15 +19,26 @@ def rgb(dataset, at_index = 0, bands = ['red', 'green', 'blue'], paint_on_mask =
         A Dataset containing at least latitude and longitude coordinates and optionally time.
         The coordinate order should be time, latitude, and finally longitude.
         Must contain the data variables specified in the `bands` parameter.
+    at_index: int
+        The time index to show.
+    x_coord, y_coord, time_coord: str
+        Names of DataArrays in `dataset_in` to use as x, y, and time coordinates.
     bands: list-like
         A list-like containing 3 names of data variables in `dataset` to use as the red, green, and blue
         bands, respectively.
-    min_possible: int
-        The minimum valid value for relevant bands according to the platform used to retrieve the data in `dataset`.
-        For example, for Landsat this is generally 0.
-    max_possible: int
-        The maximum valid value for relevant bands according to the platform used to retrieve the data in `dataset`.
-        For example, for Landsat this is generally 10000.
+    min_possible, max_possible: int
+        The minimum and maximum valid values for the selected bands according to
+        the platform used to retrieve the data in `dataset`.
+        For example, for Landsat these are generally 0 and 10000, respectively.
+    use_data_min: bool
+        Whether to use `min_possible` or the minimum among all selected bands
+        as the band value which has a minimal intensity.
+    use_data_max: bool
+        Whether to use `max_possible` or the maximum among all selected bands
+        as the band value which has a maximal intensity.
+    min_inten, max_inten: float
+        The min and max intensities for any band. These can be in range [0,1].
+        These can be used to brighten or darken the image.
     width: int
         The width of the figure in inches.
     fig: matplotlib.figure.Figure
@@ -43,7 +57,10 @@ def rgb(dataset, at_index = 0, bands = ['red', 'green', 'blue'], paint_on_mask =
                     dataset[bands[1]],
                     dataset[bands[2]]], axis = -1)
     # Interpolate values to be in the range [0,1] for creating the image.
-    rgb = np.interp(rgb, (np.nanmin(rgb), np.nanmax(rgb)), [0,1])
+    min_rgb = np.nanmin(rgb) if use_data_min else min_possible
+    max_rgb = np.nanmax(rgb) if use_data_max else max_possible
+    rgb = np.interp(rgb, (min_rgb, max_rgb), [min_inten,max_inten])
+    rgb = rgb.astype(float)
     ### > 
     
     ### < takes a T/F mask, apply a color to T areas  
@@ -53,7 +70,7 @@ def rgb(dataset, at_index = 0, bands = ['red', 'green', 'blue'], paint_on_mask =
     
     fig, ax = retrieve_or_create_fig_ax(fig, ax, figsize=figure_ratio(rgb.shape[:2], fixed_width = width))
 
-    xarray_set_axes_labels(dataset, ax)
+    xarray_set_axes_labels(dataset, ax, x_coord, y_coord)
    
     if 'time' in dataset.dims:
         ax.imshow(rgb[at_index])
