67e294b5a4eb1dd1b25318cf2b96f8cd14696b4c
John Rattz <john.c.rattz@ama-inc.com>
2019-05-06 17:14:04 -0400

    Added raster_filter.py, containing lone_object_filter(), which reduces noise by using a modal filter.

---
+++ data_cube_utilities/raster_filter.py
@@ -0,0 +1,48 @@
+import numpy as np
+import xarray as xr
+from .clean_mask import create_circular_mask
+from skimage.filters.rank import modal
+from skimage.morphology import remove_small_objects
+
+def lone_object_filter(image, min_size=2, connectivity=1, kernel_size=3):
+    """
+    Replaces isolated, contiguous regions of values with values representing
+    the surrounding pixels.
+    More specifically, this reduces noise in a raster by setting
+    contiguous regions of values of a specified maximum size to
+    the modal value in a specified neighborhood.
+    The default argument values filter out lone, singular pixels.
+    This filter is not idempotent, so it may need to be applied repeatedly
+    until the output stops changing or the results are acceptable.
+
+    Args:
+        image (numpy.array): The image to filter.
+        min_size (int): Defines the minimum number of contiguous pixels that
+                        will not be filtered out.
+        connectivity (int): The maximum distance between any two pixels such
+                            that they are considered one group.
+                            For example, a connectivity of 1 considers only
+                            adjacent value to be within one group, but a
+                            connectivity of 2 also considers diagonally
+                            connected values to be within one group.
+        kernel_size (numeric):
+                            The diameter of the circular kernel to use for
+                            filling NaN values with a modal filter.
+                            If more isolated, contiguous regions
+
+    Returns:
+        The filtered image.
+
+    Authors:
+        Andrew Lubawy (andrew.m.lubawy@ama-inc.com)
+        John Rattz    (john.c.rattz@ama-inc.com)
+    """
+    # median_filtered = median_filter(image, size=median_size)
+    modal_filtered = modal(image, create_circular_mask(kernel_size, kernel_size))
+
+    da = xr.DataArray(image)
+    for i, val in enumerate(np.unique(image)):
+        layer = remove_small_objects(image == val, min_size=min_size, connectivity=connectivity)
+        filtered = da.where(layer) if i == 0 else filtered.combine_first(da.where(layer))
+    filtered.values[np.isnan(filtered.values)] = modal_filtered[np.isnan(filtered.values)]
+    return filtered.values
