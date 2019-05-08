951aded62a69da6af28f8f81b9e318c770c33f84
John Rattz <otto.wagner@ama-inc.com>
2019-05-08 11:16:09 -0400

    raster_filter.py: Formatted and added to docstring for lone_object_filter().

--- data_cube_utilities/raster_filter.py
+++ data_cube_utilities/raster_filter.py
@@ -16,25 +16,27 @@ def lone_object_filter(image, min_size=2, connectivity=1, kernel_size=3):
     until the output stops changing or the results are acceptable.
 
     Args:
-        image (numpy.array): The image to filter.
-        min_size (int): Defines the minimum number of contiguous pixels that
-                        will not be filtered out.
-        connectivity (int): The maximum distance between any two pixels such
-                            that they are considered one group.
-                            For example, a connectivity of 1 considers only
-                            adjacent value to be within one group, but a
-                            connectivity of 2 also considers diagonally
-                            connected values to be within one group.
-        kernel_size (numeric):
-                            The diameter of the circular kernel to use for
-                            filling NaN values with a modal filter.
-                            If more isolated, contiguous regions
+        image (numpy.ndarray):
+            The image to filter.
+        min_size (int):
+            Defines the minimum number of contiguous pixels that
+            will not be filtered out.
+        connectivity (int):
+            The maximum distance between any two pixels such that they are
+            considered one group. For example, a connectivity of 1 considers
+            only adjacent values to be within one group, but a connectivity of 2
+            also considers diagonally connected values to be within one group.
+        kernel_size (int or float):
+            The diameter of the circular kernel to use for the modal filter.
+            If there are still large, contiguous regions of lone pixels,
+            increase this value to remove them. Note that the larger this value
+            is, the more detail may be lost in the image.
 
     Returns:
         The filtered image.
 
     Authors:
-        Andrew Lubawy (andrew.m.lubawy@ama-inc.com)
+        Andrew Lubawy (andrew.m.lubawy@ama-inc.com)\n
         John Rattz    (john.c.rattz@ama-inc.com)
     """
     # median_filtered = median_filter(image, size=median_size)
